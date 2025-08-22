1b33522ea767dc61a13154fd7d664c2576aa55f8
vulh <vubk2017@gmail.com>
2025-08-22 11:29:50 +0700

    Implement function remove sub folders

--- src/_1233_remove_sub_folder_from_file_system/RemoveFoldersFromFileSystem.java
+++ src/_1233_remove_sub_folder_from_file_system/RemoveFoldersFromFileSystem.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class RemoveFoldersFromFileSystem {
     public static void main(String[] args) {
 
-        String[] folder = {"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"};
-        List<String> output = removeSubfolders(folder);
+        String[] folder = {"a","/a", "/a/b", "/c/d", "/c/d/e", "/c/f"};
+        List<String> output = removeFolder(folder);
         System.out.println(output); // Output: [/a, /c/d, /c/f]
 
     }
@@ -48,4 +48,30 @@ public class RemoveFoldersFromFileSystem {
 
         return result;
     }
+
+    public static List<String> removeFolder (String[] folders) {
+        Arrays.sort(folders);
+        List<String> result = new ArrayList<>();
+        result.add(folders[0]);
+
+        for (int i = 1 ; i < folders.length; i++) {
+            String lastFolder = result.get(result.size() - 1);
+            lastFolder += '/';
+            if (!folders[i].startsWith(lastFolder)) {
+                result.add(folders[i]);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * given a list of folder, return the folder after removing all sub-folder in those folders , you may return the answer in any folder.
+     * if a folders [i] is located within another folder[j] , it is called a folder-sub of it. A sub - folder of folder[j]
+     * must start with folder[j] , followed by a "/" , For example "/a/b" is a sub-folders of "/a" but "/b" is not a sub-folder of
+     * "a/b/c"
+
+     */
+
+
 }
